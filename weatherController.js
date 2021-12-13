4cd6128a5f2b8cdf8c60538d1d14d67ebb480f2a
necmigunduz <necmigunduz@gmail.com>
2021-12-13 16:41:34 +0300

    Add create weather

--- weatherController.js
+++ weatherController.js
@@ -16,3 +16,22 @@ exports.index = function(req, res) {
         })
     })
 }
+
+// Handle create weather conditions
+exports.new = function(req, res) {
+    var weather = Weather.new()
+    weather.cityName = req.body.cityName ? req.body.cityName : weather.cityName
+    weather.weatherType = req.body.weatherType
+    weather.temperature = req.body.temperature
+    // Save the weather
+        weather.save(function(err){
+            if (err)
+                res.json(err)
+            res.json({
+                message: 'New weather is created!',
+                data: weather
+            })
+        })
+}
+
+// Handle weather info
--- weatherModel.js
+++ weatherModel.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose')
 var weatherSchema = mongoose.Schema({
     cityName: string,
     weatherType: string,
-    temparature: integer
+    temperature: integer
 })
 
 // Export the model
